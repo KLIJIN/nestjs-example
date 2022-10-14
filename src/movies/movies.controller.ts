e14a6e3f8b37e14868ac3b18d16ed3dc53f1d309
Dmitry.Zykov@evraz.com <zykov_ds@sib.evraz.com>
2022-10-14 11:39:56 +0700

    add param in requests

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('/movies')
 export class MoviesController {
@@ -7,18 +16,31 @@ export class MoviesController {
     return 'return all movies';
   }
 
+  @Get('/search')
+  findAll(@Query() query) {
+    return `you search ${query.year} width title: ${query.title}`;
+  }
+
   @Get('/:id')
   getOne(@Param('id') id: string) {
     return `вы зашли в ${id} фильм`;
   }
 
   @Post()
-  createMovie() {
-    return 'ты создал фильм';
+  createMovie(@Body() body) {
+    return body;
   }
 
   @Delete('/:id')
   deleteMovie(@Param('id') id: string) {
     return `ты удалил фильм ${id} :( `;
+  }
+
+  @Patch('/:id')
+  updateMovie(@Param('id') id: string, @Body() body) {
+    return {
+      updatedMovie: id,
+      ...body,
+    };
   }
 }
